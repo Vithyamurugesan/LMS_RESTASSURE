ee0d6ef34fd05701a225df44693bb4562a581271
Vithyamurugesan <vithyabtech54@gmail.com>
2026-06-22 03:41:56 +0530

    Test implemented

---
+++ LMS_RESTASSURE/src/test/java/com/test/HealthCheckTest.java
@@ -0,0 +1,21 @@
+package com.test;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import io.restassured.RestAssured;
+import io.restassured.response.Response;
+
+
+public class HealthCheckTest extends BaseTest {
+
+    @Test
+    public void healthCheck() {
+
+        Response response = RestAssured
+                .given()
+                .when()
+                .get(BASE_URL + "/");
+        
+        Assert.assertEquals(response.getStatusCode(), 200);
+    }
+}
